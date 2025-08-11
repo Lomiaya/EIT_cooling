afa5ee48ed1fcbf6df957ed8edca23e421274400
Lomiaya <lomiaya@163.com>
2025-08-11 13:45:14 -0400

    fixing lvalue & to rvalue

--- examples/simulation_caf.cpp
+++ examples/simulation_caf.cpp
@@ -21,7 +21,7 @@ void write_to_file(std::string file_name, const Eigen::MatrixXd& data) {
 int main() {
     States states = define_states();
     Params params = create_params(states);
-    auto& [tot_jumps, avg_tempsx, avg_tempsz] = simulation::simulate(params, states, 5000001, 1000e-6, 10);
+    auto [tot_jumps, avg_tempsx, avg_tempsz] = simulation::simulate(params, states, 5000001, 1000e-6, 10);
     write_to_file("./tot_jumps.txt", tot_jumps);
     write_to_file("./avg_tempsx.txt", avg_tempsx);
     write_to_file("./avg_tempsz.txt", avg_tempsz);
