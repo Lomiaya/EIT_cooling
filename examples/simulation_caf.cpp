5f72dbd2ab8bfe7e895c9ac94866cb0ec78e8e4d
Lomiaya <lomiaya@163.com>
2025-08-21 10:28:57 -0400

    21+ cooling; simulation reverted to absolute zero b field

--- examples/simulation_caf.cpp
+++ examples/simulation_caf.cpp
@@ -20,13 +20,13 @@ void write_to_file(std::string file_name, std::string label, const Eigen::Matrix
 
 int main() {
     for (int i = 0; i < 16; ++i) {
-        std::cout << "Running simulation, B = " << (i-0.01) / 16.0 << "G..." << std::endl;
-        States states = define_states((i-0.01) / 16.0); // Vary B-field from -0 to 1 Gauss
+        std::cout << "Running simulation, B = " << (i-0.0) / 16.0 << "G..." << std::endl;
+        States states = define_states((i-0.0) / 16.0); // Vary B-field from -0 to 1 Gauss
         Params params = create_params(states);
         auto [tot_jumps, avg_tempsx, avg_tempsz] = simulation::simulate(params, states, 400001, 10000e-6, 10, 1e6);
-        write_to_file("./tot_jumps.txt", "B = " + std::to_string((i-0.01) / 16.0), tot_jumps);
-        write_to_file("./avg_tempsx.txt", "B = " + std::to_string((i-0.01) / 16.0), avg_tempsx);
-        write_to_file("./avg_tempsz.txt", "B = " + std::to_string((i-0.01) / 16.0), avg_tempsz);
+        write_to_file("./tot_jumps.txt", "B = " + std::to_string((i-0.0) / 16.0), tot_jumps);
+        write_to_file("./avg_tempsx.txt", "B = " + std::to_string((i-0.0) / 16.0), avg_tempsx);
+        write_to_file("./avg_tempsz.txt", "B = " + std::to_string((i-0.0) / 16.0), avg_tempsz);
     }
     return 0;
 }
